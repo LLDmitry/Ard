28ba6cbbffeb781b0eb6b86082202d95bd3bdedc
LLDmitry <LLDmitry@yandex.ru>
2019-07-26 23:06:11 +0300

    receiver-sender

--- Experiments/NRF/Sender/Sender.ino
+++ Experiments/NRF/Sender/Sender.ino
@@ -23,144 +23,48 @@ bool role = 1;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("RF24/examples/GettingStarted"));
   Serial.println(F("Sender"));
 
   RadioSetup();
 
   // Start the radio listening for data
-  radio.startListening();
+  // radio.startListening();
 }
 
 void RadioSetup()
 {
   //RF24
-  //стоило переключить CS с 9 ножки в + и все бодренько заработало
-
   radio.begin();                          // Включение модуля;
   _delay_ms(2);
 
   radio.enableAckPayload();
   radio.setPayloadSize(32);
-  radio.setChannel(0x60);             // Установка канала вещания;
-  //radio.setRetries(10, 10);               // Установка интервала и количества попыток "дозвона" до приемника;
+  radio.setChannel(0x22);             // Установка канала вещания;
+  radio.setRetries(10, 10);               // Установка интервала и количества попыток "дозвона" до приемника;
   radio.setDataRate(RF24_1MBPS);            // Установка скорости(RF24_250KBPS, RF24_1MBPS или RF24_2MBPS), RF24_250KBPS на nRF24L01 (без +) неработает.
-  radio.setPALevel(RF24_PA_MAX);            // Установка максимальной мощности;
-  //radio.setPALevel(RF24_PA_LOW);
+  //radio.setPALevel(RF24_PA_MAX);            // Установка максимальной мощности;
+  radio.setPALevel(RF24_PA_LOW);
 
-  //  if (radioNumber) {
-  //    radio.openWritingPipe(addresses[1]);
-  //    radio.openReadingPipe(1, addresses[0]);
-  //  } else {
-  //    radio.openWritingPipe(addresses[0]);
-  //    radio.openReadingPipe(1, addresses[1]);
-  //  }
+  radio.openWritingPipe(123);
+  radio.openReadingPipe(1, 124);
 
-  if (radioNumber) {
-    radio.openReadingPipe(1, CentralReadingPipe);
-    radio.openWritingPipe(ArRoomsReadingPipes[ROOM_SENSOR]);
-  }
-  else
-  {
-    radio.openWritingPipe(CentralReadingPipe);
-    radio.openReadingPipe(1, ArRoomsReadingPipes[ROOM_SENSOR]);
-  }
+  radio.stopListening();
 
   radio.printDetails();
 }
 
 void loop() {
+  Serial.println(F("Now sending"));
+  radio.startListening();
+  radio.stopListening();
+  unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
+  if (!radio.write( &start_time, sizeof(unsigned long) ))
+    Serial.println("failed");
+  else
+    Serial.println("SUCCESS");
 
+  Serial.print(F("Sent "));
+  Serial.println(start_time);
 
-  /****************** Ping Out Role ***************************/
-  if (role == 1)  {
-
-    radio.stopListening();                                    // First, stop listening so we can talk.
-
-    //radio.enableAckPayload();
-
-    Serial.println(F("Now sending"));
-
-    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-    if (!radio.write( &start_time, sizeof(unsigned long) )) {
-      Serial.println(F("failed"));
-    }
-
-    radio.startListening();                                    // Now, continue listening
-
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-
-    while ( ! radio.available() ) {                            // While nothing is received
-      if (micros() - started_waiting_at > 200000 ) {           // If waited longer than 200ms, indicate timeout and exit while loop
-        timeout = true;
-        break;
-      }
-    }
-
-    if ( timeout ) {                                            // Describe the results
-      Serial.println(F("Failed, response timed out."));
-    } else {
-      unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-      radio.read( &got_time, sizeof(unsigned long) );
-      unsigned long end_time = micros();
-
-      // Spew it
-      Serial.print(F("Sent "));
-      Serial.print(start_time);
-      Serial.print(F(", Got response "));
-      Serial.print(got_time);
-      Serial.print(F(", Round-trip delay "));
-      Serial.print(end_time - start_time);
-      Serial.println(F(" microseconds"));
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-
-
-  /****************** Pong Back Role ***************************/
-
-  if ( role == 0 )
-  {
-    unsigned long got_time;
-
-    if ( radio.available()) {
-      // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }
-
-      radio.stopListening();                                        // First, stop listening so we can talk
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);
-    }
-  }
-
-
-
-  //
-  ///****************** Change Roles via Serial Commands ***************************/
-  //
-  //  if ( Serial.available() )
-  //  {
-  //    char c = toupper(Serial.read());
-  //    if ( c == 'T' && role == 0 ){
-  //      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-  //      role = 1;                  // Become the primary transmitter (ping out)
-  //
-  //   }else
-  //    if ( c == 'R' && role == 1 ){
-  //      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));
-  //       role = 0;                // Become the primary receiver (pong back)
-  //       radio.startListening();
-  //
-  //    }
-  //  }
-
-
+  delay(2000);
 } // Loop
